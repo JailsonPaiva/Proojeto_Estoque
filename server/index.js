5d12f0ac50c59842006b4d495aa4a6354955c353
jailson paiva <jailsonp437@gmail.com>
2023-05-08 15:17:31 -0400

    backend iniciado, rota cadastrar ordem criada

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host:"concipe.com.br",
+    user:"concipecom_fasiclin",
+    password:"db_aluno2023",
+    database: "concipecom_fasiclin"
+
+});
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/confirmar", (req, res) =>{
+    const { ordem } = req.body;
+    const { fornecedor } =  req.body;
+    const { cnpj } =  req.body;
+    const { entrega } =  req.body;
+    const { descri } =  req.body;
+    const { qtd } =  req.body;
+    const { valor } =  req.body;
+
+    let SQL = "INSERT INTO estoque (ordem, cnpj, fornecedor, entrega, valor, qtd, descri) VALUES (?,?,?,?,?,?,?)";
+
+    db.query(SQL, [ordem, cnpj, fornecedor, entrega, valor, qtd, descri], (err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log("tudo certo")
+        }
+    });
+
+});
+
+
+
+app.listen(8080, () => {
+    console.log("Server Rodando!")
+});
--- src/pages/CadastrarOrdem/CadastrarOrdem.jsx
+++ src/pages/CadastrarOrdem/CadastrarOrdem.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import Axios from "axios";
+
 import Header from '../../components/Header';
 
 import { Link } from 'react-router-dom'
@@ -9,6 +12,30 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 function CadastrarOrdem() {
+  const [values, setValues] = useState();
+  // console.log(values)
+  const handleChangeValues = (value) => {
+    setValues((data) => ({
+      ...data,
+      [value.target.name]: value.target.value,
+    }))
+  }
+
+  const handleClickProximo = () =>{
+    Axios.post("http://localhost:8080/confirmar", {
+      // ordem, cnpj, fornecedor, entrega, valor, qtd, descri
+      ordem: values.numOrdem,
+      fornecedor: values.razaoSocial,
+      cnpj: values.cnpj,
+      entrega: values.data,
+      descri: values.desc,
+      qtd: values.qtdTotal,
+      valor: values.valor
+    }).then((response) => {
+      console.log(response)
+    });
+  };
+
   return (
    <>
     <Header />
@@ -20,47 +47,47 @@ function CadastrarOrdem() {
       <Form>
       <Row className="mb-3">
         <Form.Group as={Col} xs={4} controlId="formGridEmail">
-          <Form.Label>Nº da ordem</Form.Label>
-          <Form.Control type="text" placeholder="Digite o número da ordem" required/>
+          <Form.Label >Nº da ordem</Form.Label>
+          <Form.Control onChange={handleChangeValues} name="numOrdem" type="text" placeholder="Digite o número da ordem" required/>
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Razão Social</Form.Label>
-          <Form.Control type="text" placeholder="Razão social LTDA" />
+          <Form.Control onChange={handleChangeValues} name="razaoSocial" type="text" placeholder="Razão social LTDA" />
         </Form.Group>
       </Row>
 
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridCity">
           <Form.Label>CNPJ</Form.Label>
-          <Form.Control type="text" placeholder='CNPJ do fornecedor' required/>
+          <Form.Control onChange={handleChangeValues} name='cnpj'  type="text" placeholder='CNPJ do fornecedor' required/>
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridState">
           <Form.Label>Data da Entrega</Form.Label>
-          <Form.Control type="date" />
+          <Form.Control onChange={handleChangeValues} name='data'  type="text" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formFile">
           <Form.Label>Arquivo da ordem</Form.Label>
-          <Form.Control type='file' required/>
+          <Form.Control onChange={handleChangeValues} name='arquivo' type='file' required/>
         </Form.Group>
       </Row>
 
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridCity">
           <Form.Label>Descrição</Form.Label>
-          <Form.Control type="text" placeholder='Descreva a ordem recebida'/>
+          <Form.Control onChange={handleChangeValues} name='desc' type="text" placeholder='Descreva a ordem recebida'/>
         </Form.Group>
 
         <Form.Group as={Col} xs={3} controlId="formGridState">
           <Form.Label>Qtd Total</Form.Label>
-          <Form.Control type="number"  placeholder='Quantidade total da ordem'/>
+          <Form.Control onChange={handleChangeValues} name='qtdTotal' type="text"  placeholder='Quantidade total da ordem'/>
         </Form.Group>
 
         <Form.Group as={Col} xs={2} controlId="formFile">
           <Form.Label>Valor</Form.Label>
-          <Form.Control type='number' placeholder='R$ 0,00'/>
+          <Form.Control onChange={handleChangeValues} name='valor' type='text' placeholder='R$ 0,00'/>
         </Form.Group>
       </Row>
     
@@ -71,7 +98,7 @@ function CadastrarOrdem() {
 
       <div className={styles.divButton}>
 
-        <Button variant="success">
+        <Button variant="success" onClick={handleClickProximo}>
           <Link to="/confirmar" className={styles.link}>Próximo</Link>
         </Button>
          
